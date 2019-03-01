40c62ce8eee180c85e1881b488a305571b69afdb
Jon Glass <jonglass82@gmail.com>
2019-02-28 20:27:24 -0600

    add REST routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,7 @@
 class Api::ProductsController < ApplicationController
 
 
-  def show
+  def index
 
     @products = Product.all
 
@@ -16,6 +16,29 @@ class Api::ProductsController < ApplicationController
     puts @results
 
     render "search_results.json.jbuilder"
+
+  end
+
+
+  def create
+
+    puts "using the create method"
+
+    @name = params["name"]
+    @price = params["price"]
+    @image_url = params["image_url"]
+    @description = params["description"]
+
+
+    # @new_product = Product.create(name: @name, price: @price, image_url: @image_url , description: @description) 
+    
+  end
+
+  def update
+    @product = Product.find_by_id(:id)
+  end
+
+  def destroy
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  get "/products" => "api/products#show" 
+  get "/products" => "api/products#index" 
 
   get "/search_products/:search_id" => "api/products#find_by_title"
+
+  # post "/search_products/:search_id" => "api/products#find_by_title"
+
+  post "/products" => "api/products#create"
 
 end
